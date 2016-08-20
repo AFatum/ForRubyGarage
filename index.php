6118e5498f106af700a226875c88e54d847bd2b8
tony <kopanewanton@gmail.com>
2016-08-20 15:02:47 +0300

    Завершено формирование формы с выпадающими списками.

--- index.php
+++ index.php
@@ -101,23 +101,79 @@
         }
         if($_GET['id'] == "SQLtask")
         {
-            echo "<div class='genMod sqlMod'><div class='listName'>SQL Task</div>
-                    <div class='newListName'>
-                        <div class='topNewList byOrder'>
-                        
-                        <form action='inc/oper.php' method='post'>
+            echo "<div class='genMod sqlMod'><div class='listName sqlMod2'>SQL Task</div>
+                    <div class='newListName sqlMod2'>
+                        <form action='inc/SQLtask.php' method='post'>
                         <select name='GetOrder'>
                             <option value='alpha'>Get all statuses, not repeating, alphabetically ordered</option>
                             <option value='cntEachPro'>Get the count of all tasks in each project, order by tasks count descending</option>
                             <option value='cntEachNms'>Get the count of all tasks in each project, order by projects names</option>
                             <option value='dupTsk'>Get the list of tasks with duplicate names. Order alphabetically</option>
-                            <option value='Garage'>Get the list of tasks having several exact matches of both name and status, from the project \'Garage\'</option>
+                            <option value='Garage'>Get the list of tasks having several exact matches of both name and status, from the project 'Garage'</option>
                             <option value='more10'>Get the list of project names having more than 10 tasks in status ‘completed’. Order by project_id</option>
                         </select> 
-                        <input class = 'AddTaskBut' type='submit' value='Add Task'></form>
-                    </div></div>
+                        <input class = 'AddTaskBut update sqlMod3' type='submit' value='Go' name='Get1'><br>
+                        <div class='letter'>Get the tasks for all projects having the name beginning with <select name='beginLetter'> 
+                            <option value='a'>'A'</option>
+                            <option value='b'>'B'</option>
+                            <option value='c'>'C'</option>
+                            <option value='d'>'D'</option>
+                            <option value='e'>'E'</option>
+                            <option value='f'>'F'</option>
+                            <option value='g'>'G'</option>
+                            <option value='h'>'H'</option>
+                            <option value='i'>'I'</option>
+                            <option value='j'>'J'</option>
+                            <option value='k'>'K'</option>
+                            <option value='l'>'L'</option>
+                            <option value='m'>'M'</option>
+                            <option value='n'>'N'</option>
+                            <option value='o'>'O'</option>
+                            <option value='p'>'P'</option>
+                            <option value='q'>'Q'</option>
+                            <option value='r'>'R'</option>
+                            <option value='s'>'S'</option>
+                            <option value='t'>'T'</option>
+                            <option value='u'>'U'</option>
+                            <option value='v'>'V'</option>
+                            <option value='w'>'W'</option>
+                            <option value='x'>'X'</option>
+                            <option value='y'>'Y'</option>
+                            <option value='z'>'Z'</option>
+                        </select> letter
+                        <input class = 'AddTaskBut update sqlMod3' type='submit' value='Go' name='Get2'></div>
+                        <div class='letter letter2'>Get the list of all projects containing the <select name='middleLetter'> 
+                            <option value='a'>'a'</option>
+                            <option value='b'>'b'</option>
+                            <option value='c'>'c'</option>
+                            <option value='d'>'d'</option>
+                            <option value='e'>'e'</option>
+                            <option value='f'>'f'</option>
+                            <option value='g'>'g'</option>
+                            <option value='h'>'h'</option>
+                            <option value='i'>'i'</option>
+                            <option value='j'>'j'</option>
+                            <option value='k'>'k'</option>
+                            <option value='l'>'l'</option>
+                            <option value='m'>'m'</option>
+                            <option value='n'>'n'</option>
+                            <option value='o'>'o'</option>
+                            <option value='p'>'p'</option>
+                            <option value='q'>'q'</option>
+                            <option value='r'>'r'</option>
+                            <option value='s'>'s'</option>
+                            <option value='t'>'t'</option>
+                            <option value='u'>'u'</option>
+                            <option value='v'>'v'</option>
+                            <option value='w'>'w'</option>
+                            <option value='x'>'x'</option>
+                            <option value='y'>'y'</option>
+                            <option value='z'>'z'</option>
+                        </select> letter in the middle of the name.
+                        <input class = 'AddTaskBut update sqlMod3' type='submit' value='Go' name='Get3'></div>
+                        </form>
+                    </div>
             
             </div>";
         }
-
 ?>
