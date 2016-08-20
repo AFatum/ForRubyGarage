7181005ea4764bbd9cd6478b820d1e30b2f14790
tony <kopanewanton@gmail.com>
2016-08-20 23:09:58 +0300

    - Разработка SQL-запроса по количеству задач в листе

--- inc/SQLtask.php
+++ inc/SQLtask.php
@@ -6,9 +6,40 @@ if($_POST['GetOrder'])
 {
     switch($_POST['GetOrder'])
     {
-        case "alpha":
-            
+        case "cntEachPro":
+            $tasks = selectTasks();
+            $lists = selectList();
+            $result = array();
+            $idPro = 0;
+            $cnt = 0;
+            foreach($lists as $lst)
+            {
+                foreach($tasks as $tsk)
+                {
+                    if($lst['id'] == $tsk['project_id'])
+                    {
+                        if($idPro != $tsk['project_id']) 
+                        {
+                            $idPro = $tsk['project_id'];
+                            $cnt = 0;
+                            //$result[0]['idPro'] = $tsk['project_id'];
+                        }
+                        $result[$idPro][$tsk['id']] = $tsk['name'];
+                        $cnt++;
+                        if($idPro > 0) 
+                        {
+                            $result[$idPro]['cnt'] = $cnt;
+                            $result[$idPro]['listName'] = $lst['name'];
+                            $_SESSION['SQL'] = "cntEachPro";
+                        }
+                        
+                    }
+                }                
+            }
         break;
     }
+    $_SESSION['res'] = $result;
+    header("Location: http://ruby.ua/index.php?id=SQLtask");
+    exit;
 }
 ?>
