a22d620d6b95d7baee8a159c4dd3fa7c3e5b3011
AFatum <kopanewanton@gmail.com>
2016-08-26 00:33:23 +0300

    mo db

--- inc/db.php
+++ inc/db.php
@@ -2,11 +2,25 @@
 ob_start();		// включаем буферизацию вывода
 session_start(); // стартуем сессию
 header ('Content-Type: text/html;charset=utf-8');
-define ('DB_HOST', 'localhost');
-define ('DB_LOGIN', 'root');
-define ('DB_PASS', '');
-define ('DB_NAME', 'ruby');
-define ('ORDERS_LOG', 'orders.txt');
 
-$link = mysqli_connect(DB_HOST, DB_LOGIN, DB_PASS, DB_NAME) or die('Ошибка при соединении: '.mysqli_connect_error());
+
+if ($_SERVER['SERVER_NAME'] == "stormy-river-47352.herokuapp.com") 
+{
+	$urlGlob = parse_url(getenv("CLEARDB_DATABASE_URL"));
+	define ('DB_HOST',  $urlGlob["host"]);
+	define ('DB_LOGIN', $urlGlob["user"]);
+	define ('DB_PASS', $urlGlob["pass"]);
+	define ('DB_NAME', substr($urlGlob["path"], 1));
+	define ('ORDERS_LOG', 'orders.txt');
+	$link = mysqli_connect(DB_HOST, DB_LOGIN, DB_PASS, DB_NAME) or die('Ошибка при соединении: '.mysqli_connect_error());
+}
+else
+{
+	define ('DB_HOST', 'localhost');
+	define ('DB_LOGIN', 'root');
+	define ('DB_PASS', '');
+	define ('DB_NAME', 'ruby');
+	define ('ORDERS_LOG', 'orders.txt');
+	$link = mysqli_connect(DB_HOST, DB_LOGIN, DB_PASS, DB_NAME) or die('Ошибка при соединении: '.mysqli_connect_error());
+}
 ?>
