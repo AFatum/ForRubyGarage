31eb4daad1d838defbded77f61cea5c028b75622
Tony <kopanewanton@gmail.com>
2017-01-21 23:40:40 +0200

    есть метод newTask

--- inc/oper.class.php
+++ inc/oper.class.php
@@ -4,6 +4,12 @@ class Oper
   const HOST = LINK_HOST;
   public $db;
 
+  // вносим в конструктор класс базы данных
+  function __construct(mysqli $db)
+  {
+    $this->db = $db;
+  }
+  
   // подгототавливаем строку к внесению в бд
   function clear($data)
   {
@@ -11,29 +17,101 @@ class Oper
     return $this->db->real_escape_string(trim(strip_tags($Data)));
   }
   
-  // вносим в конструктор класс базы данных
-  function __construct(myDb $db, $type)
+  function newList($name) // ** - создаём новый лист:
+  { 
+   // *1 - подготавливаем данные для имени листа
+      $name = $this->clear($name);
+      // *2 - создаём запрос для БД
+      $sql = "INSERT INTO projects(name) VALUES (?)";
+      // *3 - создаём подготовленный запрос 
+      if(!$stmt = $this->db->prepare($sql))
+      { // *3.1 - если неудача, кидаем исключение
+        throw new Exception("Ошибка подготовленного запроса при создании листа: ".$stmt->errno." - ".$stmt->error);
+        return false;
+      }
+      // *3.2 - задаём параметры в подготовленный запрос
+      $stmt->bind_param('s', $name);
+      // *3.3 - исполняем подготовленный запрос
+      if(!$stmt->execute())
+      {
+        throw new Exception("Какая-то ошибка в исполнении подготовленного запроса при создании нового листа: " . $stmt->errno . " - " . $stmt->error);
+        return false;
+      }
+      else 
+      { $stmt->close(); return true; }
+  } // ** новый лист создан!
+  
+  function newTask($name, $pro) // ** - создаём новое задание:
   {
-    $this->db = $db;
-  }
+    // *1 - подготавливаем данные для имени листа
+    $name = $this->clear($data);
+    $pro = (int) abs($pro);
+    // *2 - создаём запрос для БД
+    $sql = "INSERT INTO tasks(name, project_id) VALUES (?, ?)";
+    // *3 - создаём подготовленный запрос 
+    if(!$stmt = $this->db->prepare($sql))
+    { // *3.1 - если неудача, кидаем исключение
+      throw new Exception("Ошибка подготовленного запроса при создании задания: ".$stmt->errno." - ".$stmt->error);
+      return false;
+    }
+    // *3.2 - задаём параметры в подготовленный запрос
+      $stmt->bind_param('si', $name, $pro);
+      // *3.3 - исполняем подготовленный запрос
+      if(!$stmt->execute())
+      {
+        throw new Exception("Какая-то ошибка в исполнении подготовленного запроса при создании нового задания: " . $stmt->errno . " - " . $stmt->error);
+        return false;
+      }
+      else 
+      { $stmt->close(); return true; }
+  } // ** новое задание создано!
+  
   
   /* основной метод операций, через который будем всё делать
    здесь если $type == false, значит обрабатывается метод POST
-   если $type == true, значит метод GET. */
-  function getOper($oper, $type = false)
+   если $type == true, значит метод GET. 
+  function getOper($oper, $data, $type = false)
   {
     if(!$type) // обработка POST
     {
       switch($oper)
       {
-        // ** создаём новый лист:
+        // ** - создаём новый лист:
         case "newList":
-          // *1 подготавливаем данные для имени листа
+          // *1 - подготавливаем данные для имени листа
           $name = $this->clear($data);
+          // *2 - создаём запрос для БД
+          $sql = "INSERT INTO projects(name) VALUES (?)";
+          // *3 - создаём подготовленный запрос 
+          if(!$stmt = $this->db->prepare($sql))
+          { // *3.1 - если неудача, кидаем исключение
+            throw new Exception("Ошибка подготовленного запроса при создании листа: ".$stmt->errno." - ".$stmt->error);
+            return false;
+          }
+          // *3.2 - задаём параметры в подготовленный запрос
+          $stmt->bind_param('s', $name);
+          // *3.3 - исполняем подготовленный запрос
+          if(!$stmt->execute())
+          {
+            throw new Exception("Какая-то ошибка в исполнении подготовленного запроса при создании нового листа: " . $stmt->errno . " - " . $stmt->error);
+            return false;
+          }
+          else 
+          { $stmt->close(); return true; }
           return $this->db->newList($name);
         break; // ** новый лист создан!
+        
+        // ** - создаём новое задание
+        case "newTask":
+          // *1 - подготавливаем данные для имени листа
+          $name = $this->clear($data);
+          // *2 - создаём запрос для БД
+          $sql = "INSERT INTO projects(name) VALUES (?)";
       }
     }
+  */
+    
+    
     
     if($type)  // обработка GET
     {
@@ -62,20 +140,5 @@ class Oper
     
   }
 }
-
-/*function newList($name)
-{
-    global $link;
-    if(!is_string($name)) return false;
-
-    //$name = mysqli_real_escape_string ($link, trim(strip_tags($name)));
-    $sql = "INSERT INTO projects(name) VALUES (?)";
-    if(!$stmtIns = mysqli_prepare($link, $sql)) return false;
-
-    mysqli_stmt_bind_param($stmtIns, 's', $name);
-    mysqli_stmt_execute($stmtIns) or die("Какая-то ошибка в запросе ".mysqli_error($link));    
-    mysqli_stmt_close($stmtIns);
-    return true;
-}*/
   
 ?>
