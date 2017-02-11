93444b1e0f44ec1410fd83507782c825974f1b51
Tony <kopanewanton@gmail.com>
2017-02-12 01:20:41 +0200

    Некоторые изменения для соединения с heroku

--- inc/db.php
+++ inc/db.php
@@ -5,7 +5,8 @@ require("oper.class.php");
 header ('Content-Type: text/html;charset=utf-8');
 
 
-if ($_SERVER['SERVER_NAME'] == "stormy-river-47352.herokuapp.com") 
+//if ($_SERVER['SERVER_NAME'] == "stormy-river-47352.herokuapp.com") 
+if ($_SERVER['SERVER_NAME'] == "for-rubygarage-tony.herokuapp.com")
 {
 	$urlGlob = parse_url(getenv("CLEARDB_DATABASE_URL"));
 	define ('DB_HOST',  $urlGlob["host"]);
